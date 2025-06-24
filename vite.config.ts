d38b0674f43b3b6f303fdcdbf8e0816174da3a36
tanviladha <90061867+tanviladha@users.noreply.github.com>
2025-06-24 12:58:33 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { tempo } from "tempo-devtools/dist/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/tanviladha.github.io",
   optimizeDeps: {
     entries: ["src/main.tsx", "src/tempobook/**/*"],
   },
